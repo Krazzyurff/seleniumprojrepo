1b95c1a8ab0c330a47eb2264f3ab3c0289d63055
ashish8135 <ashish.shaivya10@gmail.com>
2026-03-27 00:43:35 +0530

    ABC

--- src/test/java/test/HomePage.java
+++ src/test/java/test/HomePage.java
@@ -4,14 +4,8 @@ public class HomePage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("hero");
-     System.out.println("Don");
-     System.out.println("Don");
-     System.out.println("Don");
-     System.out.println("Don");
-     
+   System.out.println("Ashish");
 	}
-	   
-		
+   	
 
 }
