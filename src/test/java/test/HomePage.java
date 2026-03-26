21aa85caf85a49cc52b25236e5d3f52c33e2167d
ashish8135 <ashish.shaivya10@gmail.com>
2026-03-27 01:26:54 +0530

    ABC

--- src/test/java/test/HomePage.java
+++ src/test/java/test/HomePage.java
@@ -3,9 +3,14 @@ package test;
 public class HomePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-   System.out.println("Ashish");
+
+		HomePage d = new HomePage();
+		int result = d.subtractNumbers(10, 4);
+		System.out.println(result);  // Output: 6
 	}
    	
+	  public int subtractNumbers(int a, int b) {
+	        return a - b;
+	    }
 
 }
