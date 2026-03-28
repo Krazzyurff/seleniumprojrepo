69782b44099e9fc97134d2cf60371406df4560fa
ashish8135 <ashish.shaivya10@gmail.com>
2026-03-29 00:56:42 +0530

    Ashish

--- src/test/java/test/HomePage.java
+++ src/test/java/test/HomePage.java
@@ -1,20 +1,9 @@
 package test;
 
 public class HomePage {
-
 	public static void main(String[] args) {
 
-		// TODO Auto-generated method stub
-
-
-   System.out.println("Ashish");
-
-     System.out.println("hero");
-     System.out.println("Don");
-     System.out.println("Don");
-     System.out.println("Don");
-     System.out.println("Don");
-     System.out.println("Ashish");
+		
 
 		HomePage d = new HomePage();
 		int result = d.subtractNumbers(10, 4);
@@ -22,6 +11,9 @@ public class HomePage {
 		HomePage b = new HomePage();
 		int result1 = b.addTwoNumbers(5, 10);
 		System.out.println(result1); 
+		HomePage c = new HomePage();
+		int result3 = d.subtractNumbers(10, 4);
+		
 	}
 	  public int subtractNumbers(int a, int b) {
 	        return a - b;
@@ -30,5 +22,6 @@ public class HomePage {
 	  public int addTwoNumbers(int a, int b) {
 	        return a + b;
 	    }
+	  
 
 }
