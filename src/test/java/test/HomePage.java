858d476877681486eb0583e669ca5dd4cd94ef8f
ashish8135 <ashish.shaivya10@gmail.com>
2026-03-29 01:02:56 +0530

    ABC

--- src/test/java/test/HomePage.java
+++ src/test/java/test/HomePage.java
@@ -7,6 +7,13 @@ public class HomePage {
 		HomePage d = new HomePage();
 		int result = d.subtractNumbers(10, 4);
 		System.out.println(result);  // Output: 6
+	       int a = 5;
+	        int b = 4;
+
+	        int result2 = a * b;
+
+	        System.out.println("Multiplication: " + result);
+	    
 	}
    	
 	  public int subtractNumbers(int a, int b) {
